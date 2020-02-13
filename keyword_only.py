804203b7bf0849cb30356a815aafed8d8ed051d5
Комлик Александр Викторович <komlik_av@st.by>
2020-02-13 17:03:17 +0300

    add files

--- keyword_only.py
+++ keyword_only.py
@@ -1,9 +1,9 @@
-def total(initial=5, *numbers, exstra_number):
+def total(initial=5, *numbers, **keywords):
     count = initial
     for number in numbers:
         count += number
         print(count)
-    count += exstra_number
+    count += keywords[key]
     print(count)
 
-total(10, 1, 2, 3, exstra_number=50)
+total(10, 1, 2, 3, vegetables=50, fruits=100)
---
+++ using_sys.py
@@ -0,0 +1,7 @@
+import sys
+
+print('аргументы  командной  строки: ')
+for i in sys.argv:
+    print(i)
+print('\n\n Переменная PYTHONPATH содержит', sys.path, '\n')
+
