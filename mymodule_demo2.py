426d99d9d6b0fb115db5ed1476519b1e4b76041f
Комлик Александр Викторович <komlik_av@st.by>
2020-03-06 16:38:47 +0300

    add  new docker

--- mymodule_demo2.py
+++ mymodule_demo2.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 from module import sayhi, __version__
 
 sayhi()
 
-=======
-python3from module import sayhi, __version__
-
-sayhi()
-
->>>>>>> a09fb2a967d0daa7fef432fffa28074655a087ec
 print('Версия', __version__)
