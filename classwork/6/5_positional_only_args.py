4c02495db3fff9d32b96d2d6c9d0f313ded0f4b7
Aliaksandr <alex@pop-os.localdomain>
2020-03-08 08:47:26 +0300

    add game

--- classwork/6/5_positional_only_args.py
+++ classwork/6/5_positional_only_args.py
@@ -1,7 +1,7 @@
-def test(a, b, /, c, *, d):
+def test(a, b, c, *, d):
     print(a, b, c)
 
 #test(a=1, b=2, c=3, d=6)
 
-test(1, 2, 3, d=6)
+test(1, 2, 3, )
 test(1,2, c=3, d=6)
--- keyword_only.py
+++ keyword_only.py
@@ -1,9 +1,9 @@
-def total(initial=5, *numbers, **keywords):
+def total(initial=5, *numbers, extra_number):
     count = initial
     for number in numbers:
         count += number
         print(count)
-    count += keywords[key]
+    count += extra_number
     print(count)
 
-total(10, 1, 2, 3, vegetables=50, fruits=100)
+total(10, 1, 2, 3, 4, extra_number=10)
